de3b8424c79b48a790bb6bc6343d1527ec0d3c7c
mlgibson8 <michael.l.gibson@du.edu>
2023-08-03 08:33:11 -0600

    connection troubles

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 const mysql = require('mysql2');
 //require('dotenv').config();
-
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
 const sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: 'mysql',
