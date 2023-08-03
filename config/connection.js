507993eb39ad0f0570ac59c89b917034054189ac
mlgibson8 <michael.l.gibson@du.edu>
2023-08-03 08:59:17 -0600

    whoever decied i had to add and commit to test depoloyments should lose their job

--- config/connection.js
+++ config/connection.js
@@ -13,7 +13,7 @@ sequelize.authenticate()
   console.log('Connection has been established successfully.');
 }).catch((error) => {
   console.error('Unable to connect to the database:', error);
-}); */
+});
 module.exports = sequelize;
 
 
