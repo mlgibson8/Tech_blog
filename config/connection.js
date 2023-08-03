059e455bf4e7c04391441e905a7446932251d004
mlgibson8 <michael.l.gibson@du.edu>
2023-08-03 08:50:56 -0600

    another pointless comment

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const mysql2 = require('mysql2');
 //require('dotenv').config();
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
-const sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
+const Sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: 'mysql',
 });
