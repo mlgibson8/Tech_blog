72dd3d430dcdea9f99a7b604a29efad563b190e1
mlgibson8 <michael.l.gibson@du.edu>
2023-08-03 08:28:47 -0600

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,36 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
+const mysql = require('mysql2');
 //require('dotenv').config();
 
-let sequelize;
+const sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: 'mysql',
+});
 
-if (process.env.JAWSDB_URL) {
+Sequelize.authenticate().then(() => {
+  console.log('Connection has been established successfully.');
+}).catch((error) => {
+  console.error('Unable to connect to the database:', error);
+});
+module.exports = sequelize;
+
+
+/* if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
-    /* {
+     {
       host: 'localhost',
       dialect: 'mysql',
       password: 'pword',
       port: 3306
-    } */
+    } 
   );
 }
 
 module.exports = sequelize;
+ */
